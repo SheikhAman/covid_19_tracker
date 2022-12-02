8bbec129f0d6fd3219ece081c2ee17f19fd902af
SheikhAman <amansk664@gmail.com>
2022-12-03 03:39:24 +0600

    covid tracker

---
+++ lib/Services/states_services.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:covid_tracker/Model/world_states_model.dart';
+import 'package:http/http.dart' as http;
+
+import 'Utilities/app_url.dart';
+
+class StatesServices {
+  Future<WorldStatesModel> fetchWorldStatesRecords() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      print(data);
+
+      return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+  Future<List<dynamic>> countriesListApi() async {
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+
+    var data;
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      print(data);
+      return data;
+    } else {
+      throw Exception('Error');
+    }
+  }
+}
